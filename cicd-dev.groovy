63dc47723e7a2f4439c63e71c952a73ca51f163a
GSUSHMA <GSUSHMA@ZOSCAN2B.pok.stglabs.ibm.com>
2026-07-23 07:31:10 -0400

    first commit

---
+++ cicd-dev.groovy
@@ -0,0 +1,14 @@
+node('linux') {
+  stage ('Poll') {
+    checkout([
+      $class: 'GitSCM', branches: [[name: '*/main']], extensions: [],
+      userRemoteConfigs: [[url: 'https://github.com/zopencommunity/rocksdbport.git']]])
+  }
+  stage('Build') {
+    build job: 'Port-Pipeline', parameters: [
+      string(name: 'PORT_GITHUB_REPO', value: 'https://github.com/zopencommunity/rocksdbport.git'),
+      string(name: 'PORT_DESCRIPTION', value: 'This code is a library that forms the core building block for a fast key-value server, especially suited for storing data on flash drives. It has a Log-Structured-Merge-Database (LSM) design with flexible tradeoffs between Write-Amplification-Factor (WAF), Read-Amplification-Factor (RAF) and Space-Amplification-Factor (SAF). It has multi-threaded compactions, making it especially suitable for storing multiple terabytes of data in a single database.'),
+      string(name: 'BUILD_LINE', value: 'DEV')
+    ]
+  }
+}
